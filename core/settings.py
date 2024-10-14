9a6824284a94d903a98d0ba46eab5ad37b0a2571
shamuratov6563 <shamuratov6563@gmail.com>
2024-10-14 20:57:27 +0500

    changes

--- core/settings.py
+++ core/settings.py
@@ -132,14 +132,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
-]
-import os
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = (BASE_DIR / "staticfiles",)
 
 
 MEDIA_URL = '/media/'
